8923c1455ded7c7287b4a78e4ce363e9a4d0419f
OMER <OMER@OMER-MacBook-Pro.local>
2015-10-31 23:30:57 +0200

    Cemberin Icinde mi Disinda mi Odevi Ilk Commit

--- Odev_1/main.swift
+++ Odev_1/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-println("Hello, World!")
 
+let a = sqrt(81.0)
+let b = sqrt(2.0)
+let r: Double = 10
+var x: Double = 0.0
+var y: Double = 0.0
+var xsonuc: Double = 0.0
+var disToplam: Double = 0.0
+var sonuc: Double = 0.0
+var icToplam: Double = 0.0
+
+for i in 0 ..< 1000000
+{
+    x = Double(arc4random_uniform(10))
+    y = Double(arc4random_uniform(10))
+    xsonuc = pow(x,2) + pow(y,2)
+    xsonuc = sqrt(xsonuc)
+    if r >= xsonuc
+    {
+        icToplam += 1
+    }
+    else
+    {
+        disToplam += 1
+    }
+}
+sonuc = icToplam / disToplam
+println("İçindekilerin Toplamı: \(icToplam)")
+println("Toplam: \(disToplam)")
+println("İçindekilerin Toplama Oranı: \(sonuc)")
